6973e684a8204c530e5c6936e2aa66ff70dca4c5
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-06-02 23:37:51 +0200

    start IRC chat project

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+require('dotenv').config({ path: './.env' });
+
+
+import express from 'express'
+import { createServer } from 'node:http'
+import path, { join } from 'path'
+import { init_database } from './database';
+import websocketServer from './websockets/index_public';
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json()
+import mysql from 'mysql2'
+
+const PORT = 3000;
+
+const app = express();
+
+const router = require('./routes/users.ts');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function startServer() {
+	console.log("startServer ")
+	app.use("/", router)
+	const server = createServer(app)
+
+	server.listen(PORT, () => { console.log(`Listen ${PORT}`) })
+}
+
+
+
+startServer()
