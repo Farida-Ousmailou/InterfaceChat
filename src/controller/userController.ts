a4f2fe979f6ed0f1c5f9e616d6c2f04bb46531e3
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-06-03 11:41:32 +0200

    completed the user controller and handle the token

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -29,4 +29,59 @@ export const createUser = async (req: Request, res: Response) => {
 			console.error('Error creating user:', error);
 			res.status(500).send('Internal Server Error');
 	}
+}
+
+export const createUser = async (req: Request, res: Response) => {
+	const newUser: User = {
+		name: req.body.name,
+		pseudo: req.body.pseudo,
+		mail: req.body.mail,
+		password: req.body.password,
+	}
+	try {
+		connection.query('INSERT INTO users SET ?', newUser);
+		if(res.status(200)){
+			res.status(200).send(`User created successfully ${newUser}`);
+		} else {
+			console.log("grosse erreur")
+		}
+	} catch (error) {
+			console.error('Error creating user:', error);
+			res.status(500).send('Internal Server Error');
+	}
 };
+
+export const login = async (req: Request, res: Response) => {
+	try {
+		const credentials: Omit<User, "name" | "pseudo"> = {
+			mail: req.body.mail,
+			password: req.body.password,
+		};
+
+	console.log('the credentials', credentials.mail, credentials.password)
+
+	const response = await connection.promise().query('SELECT * FROM users WHERE mail = ?', [credentials.mail],);
+	const DBPassword = response[0][0]["password"];
+	const DBmail = response[0][0]["mail"];
+	console.log('password', DBPassword);
+	console.log('mail', DBmail);
+
+	if(DBPassword === credentials.password && DBmail === credentials.mail) {
+		console.log("you can have your token")
+		const token =  generateAccessToken (response);
+		console.log('accessToken', token);
+		res.json({ token });
+	}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+
+ function generateAccessToken(response) {
+	const id = response[0][0].id
+	console.log('le process', process.env.TOKEN_SECRET);
+	return  jwt.sign({ id } , process.env.TOKEN_SECRET, {expiresIn: 100000000000});
+}
+
+
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,11 @@
 import mysql from 'mysql2'
 
-export function init_database() {
-    const connection = mysql.createConnection({
-        host: '127.0.0.1',
-        user: '',
-        password: '',
-        database: 'myIRC'
-      })
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+})
       
     connection.connect((error: any) => { 
           if (error){ 
