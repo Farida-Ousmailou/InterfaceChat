0fd47a0ed68ebc250200eb48e9d65c289bdfcd5e
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-06-03 14:10:28 +0200

    add some middleware

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,14 @@
 import mysql from 'mysql2'
 
-const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-})
+
+export function init_database() {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  })
+
       
     connection.connect((error: any) => { 
           if (error){ 
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import express from 'express'
 import { createServer } from 'node:http'
 import path, { join } from 'path'
 import { init_database } from './database';
-// import websocketServer from './websockets/index_public';
+import websocketServer from './websockets/socket';
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
 import mysql from 'mysql2'
@@ -22,7 +22,11 @@ function startServer() {
 	console.log("startServer ")
 	app.use("/", router)
 	init_database()
+	fontend(app)
+
 	const server = createServer(app);
+
+	websocketServer(server);
 
 
 	server.listen(PORT, () => { console.log(`Listen ${PORT}`) })
@@ -30,3 +34,29 @@ function startServer() {
 
 
 startServer()
+
+
+function fontend(app) {
+	app.use(express.static(path.join(__dirname, "public")))
+
+
+	app.get('/', (req, res) => {
+		res.sendFile(join(__dirname, 'views/signUp.html'));
+	});
+
+	app.get('/logIn.html', (req, res) => {
+		res.sendFile(join(__dirname, 'views/logIn.html'));
+	});
+
+	app.get('/accueil.html', (req, res) => {
+		res.sendFile(join(__dirname, 'views/accueil.html'));
+	});
+
+	app.get('/index.html', (req, res) => {
+		res.sendFile(join(__dirname, 'views/index.html'));
+	});
+
+	app.get("/PrivateChannel.html", (req, res) => {
+		res.sendFile(join(__dirname, 'views/PrivateChannel.html'));
+	});
+}
