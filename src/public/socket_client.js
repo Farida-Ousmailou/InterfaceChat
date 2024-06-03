2833a794b68baea6a11cec3144a4f30ae07a1fba
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-06-03 12:06:05 +0200

    adding function init on the socket client side

--- src/public/socket_client.js
+++ src/public/socket_client.js
@@ -50,4 +50,47 @@ function sendDataMessgePublic() {
     document.getElementById('body').appendChild(li);
     console.log(document.getElementById('body').appendChild(li))
   }
+
+  function init() {
+    const socket = io()
   
+    socket.on('message', (msg) => {
+      console.log('Message received: ' + msg);
+      var li = document.createElement("li");
+      var textNode = document.createTextNode(msg);
+      li.appendChild(textNode);
+      document.getElementById('message-container').appendChild(li);
+      window.scrollTo(0, document.body.scrollHeight)
+    });
+  
+  
+    document.getElementById("chat-form").addEventListener('submit', (e) => {
+      e.preventDefault();
+      const messageInput = document.getElementById('message-input');
+      const message = messageInput.value
+  
+  
+      if (message) {
+        socket.emit('chat message', { room: 'ch1', message: message });
+        messageInput.value = ""
+        console.log("rien", message)
+      }
+    });
+  
+    socket.on('connection', () => {
+      console.log('Connected to server');
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+  
+ 
+  }
+  
+  document.addEventListener("DOMContentLoaded", function (e) {
+    console.log(" page has loaded!");
+    init()
+    handleGettingPseudo()
+    handleGettingroom()
+  });
